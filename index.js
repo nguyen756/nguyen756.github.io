2ea0a6300cdcd5d0c8f47c594f685a98ee4118ee
nguyen756 <151891782+nguyen756@users.noreply.github.com>
2025-08-25 22:16:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import express from 'express'
 const app = express();
 const __dirname=import.meta.dirname;
-app.use(express.static(__dirname));
+
+app.use('/static',express.static('static'));
+
 app.get('/',function(req,res)
 {
    
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname+'/main.html');
     
 });
 app.get('/about',function(req,res){
 //res.sendFile('D:/College/_hk5/Web/demo/about.html');
 res.sendFile(__dirname+'/about.html');
 });
+
 
 app.listen(8080,function()
 {
