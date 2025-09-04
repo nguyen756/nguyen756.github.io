d9e9ab66aaec1bb9e547f442394b7fc8675265ea
nguyen756 <151891782+nguyen756@users.noreply.github.com>
2025-09-04 22:32:10 +0700

    Messing around with login/post

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ app.get('/about',function(req,res){
 //res.sendFile('D:/College/_hk5/Web/demo/about.html');
 res.sendFile(__dirname+'/about.html');
 });
-
-
+app.get('/account/signup',function(req,res){
+    res.sendFile(__dirname+'/register.html');
+});
+app.use(express.urlencoded({
+extended:true
+}));
+app.post('/account/signup',function(req,res){
+    res.send(JSON.stringify(req.body));
+});
 app.listen(3000,function()
 {
     console.log('server is running on port 3000')
